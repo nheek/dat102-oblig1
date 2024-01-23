03c3bbfa34865f9f24d2862f9b95cd70b766f2af
nheek <nickjameshipol@gmail.com>
2024-01-23 11:17:41 +0100

    fixed Filmarkiv.java

--- filmarkiv/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ filmarkiv/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,5 +1,56 @@
 package no.hvl.data102.filmarkiv.impl;
 
-public class Filmarkiv {
+import no.hvl.data102.filmarkiv.adt.FilmArkivADT;
+import no.hvl.data102.filmarkiv.impl.Sjanger;
+import no.hvl.data102.filmarkiv.impl.Film;
+public class Filmarkiv implements FilmArkivADT {
+    Film[] filmarkivTabell = new Film[] {
+        
+    };
+
+    public Filmarkiv (int antallFilm) {
+        this.filmarkivTabell = new Film[antallFilm];
+    }
+    
+    @Override
+    public Film finnFilm(int nr) {
+        throw new UnsupportedOperationException("Unimplemented method 'leggTilFilm'");
+    }
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'leggTilFilm'");
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'slettFilm'");
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'soekTittel'");
+    }
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'soekProdusent'");
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'antall'");
+    }
+
+    @Override
+    public int antall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'antall'");
+    }
 
 }
