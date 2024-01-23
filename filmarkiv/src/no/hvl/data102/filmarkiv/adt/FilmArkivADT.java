33afa604a1d0ef765e90230027e5b65597314c2d
nheek <nickjameshipol@gmail.com>
2024-01-23 11:03:35 +0100

    changed FilmArkiv.java

--- filmarkiv/src/no/hvl/data102/filmarkiv/adt/FilmArkivADT.java
+++ filmarkiv/src/no/hvl/data102/filmarkiv/adt/FilmArkivADT.java
@@ -1,5 +1,45 @@
 package no.hvl.data102.filmarkiv.adt;
 
-public class FilmArkivADT {
+import no.hvl.data102.filmarkiv.impl.Film;
 
-}
+public interface FilmArkivADT {
+    /**
+     * 
+     * @param nr
+     * @return
+     */
+    Film finnFilm(int nr);
+    /**
+    * Legger til en ny film.
+    * @param nyFilm
+    */
+    void leggTilFilm(Film nyFilm);
+    /**
+    * Sletter en fil med gitt nr
+    * @param filmnr nr på film som skal slettes
+    * @return true dersom filmen ble slettet, false ellers
+    */
+    boolean slettFilm(int filmnr);
+    /**
+    * Søker og henter Filmer med en gitt delstreng i tittelen.
+    * @param delstreng som må være i tittel
+    * @return tabell med filmer som har delstreng i tittel
+    */
+    Film[] soekTittel(String delstreng);
+     /**
+    * Søker og henter filmer med en gitt delstreng i filmprodusent
+    * @param delstreng
+    * @return
+    */
+    Film[] soekProdusent(String delstreng);
+    /**
+    * Finner antall filmer med gitt sjanger
+    * @param sjanger
+    * @return antall filmer av gitt sjanger.
+    */
+     int antall(Sjanger sjanger);
+    /**
+    * @return antall filmer i arkivet
+    */
+    int antall();
+    }
